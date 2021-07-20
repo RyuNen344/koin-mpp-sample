cb87cd639fb79752f75965657344fdb4c898e372
RyuNen344 <s1100633@outlook.com>
2021-07-20 23:34:49 +0900

    add plugin, update gradle version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.louiscad.complete-kotlin") version "1.0.0"
+}
+
 rootProject.name = "KoinMppSample"
 include(":androidApp")
 include(":shared")
