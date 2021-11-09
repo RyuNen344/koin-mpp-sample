28b4904d48159d84eb23a2dc32749cee331dde50
RyuNen344 <s1100633@outlook.com>
2021-11-10 06:57:04 +0900

    remove complete kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,10 +7,6 @@ pluginManagement {
     }
 }
 
-plugins {
-    id("com.louiscad.complete-kotlin") version "1.0.0"
-}
-
 rootProject.name = "KoinMppSample"
 include(":androidApp")
 include(":shared")
