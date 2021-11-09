c66abdad0ff96ea3a5df6798a5f22623b5190d97
RyuNen344 <s1100633@outlook.com>
2021-11-10 07:16:53 +0900

    bump library version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0-RC2")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.0-RC2")
         classpath("com.android.tools.build:gradle:4.2.2")
     }
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -32,40 +32,40 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                api("io.insert-koin:koin-core:3.1.2")
-                api("io.github.aakira:napier:1.5.0")
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1-native-mt")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
-                implementation("io.ktor:ktor-client-core:1.6.1")
-                implementation("io.ktor:ktor-client-logging:1.6.1")
-                implementation("io.ktor:ktor-client-json:1.6.1")
-                implementation("io.ktor:ktor-client-serialization:1.6.1")
+                api("io.insert-koin:koin-core:3.1.3")
+                api("io.github.aakira:napier:2.1.0")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+                implementation("io.ktor:ktor-client-core:1.6.4")
+                implementation("io.ktor:ktor-client-logging:1.6.4")
+                implementation("io.ktor:ktor-client-json:1.6.4")
+                implementation("io.ktor:ktor-client-serialization:1.6.4")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
-                implementation("io.insert-koin:koin-test:3.1.2")
+                implementation("io.insert-koin:koin-test:3.1.3")
             }
         }
         val androidMain by getting {
             dependencies {
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1-native-mt")
-                implementation("io.ktor:ktor-client-okhttp:1.6.1")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2-native-mt")
+                implementation("io.ktor:ktor-client-okhttp:1.6.4")
             }
         }
         val androidTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
                 implementation("junit:junit:4.13.2")
-                implementation("io.insert-koin:koin-test-junit4:3.1.2")
+                implementation("io.insert-koin:koin-test-junit4:3.1.3")
                 implementation("io.mockk:mockk:1.12.0")
             }
         }
         val iosMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-client-ios:1.6.1")
+                implementation("io.ktor:ktor-client-ios:1.6.4")
             }
         }
         val iosTest by getting
@@ -74,9 +74,9 @@ kotlin {
     // Configure the framework which is generated internally by cocoapods plugin
     targets.withType<KotlinNativeTarget> {
         binaries.withType<Framework> {
-            export("io.insert-koin:koin-core:3.1.2")
-            export("io.github.aakira:napier:1.5.0")
-            export("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1-native-mt")
+            export("io.insert-koin:koin-core:3.1.3")
+            export("io.github.aakira:napier:2.1.0")
+            export("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
         }
     }
 }
