b89ba856169c8d51f1721339c64c46ca3a0b577b
RyuNen344 <s1100633@outlook.com>
2021-11-15 01:39:01 +0900

    叩き方ひどいけどAPI叩くところまで

--- shared/src/iosMain/kotlin/com/ryunen344/koin/mpp/sample/koin/KoinExt.kt
+++ shared/src/iosMain/kotlin/com/ryunen344/koin/mpp/sample/koin/KoinExt.kt
@@ -17,9 +17,11 @@ import org.koin.core.qualifier.Qualifier
 import org.koin.core.qualifier.StringQualifier
 import org.koin.core.qualifier.TypeQualifier
 
+@OptIn(ExperimentalStdlibApi::class)
 @Suppress("UNUSED")
 fun initKoin() : KoinApplication {
-    return startKoin {
+        println("isExperimentalMM ${isExperimentalMM()}")
+        return startKoin {
         napierLogger()
         modules(
             apiModule,
