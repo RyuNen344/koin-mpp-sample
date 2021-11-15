472472bc5137a0a14d3fdce5242641119993cef2
RyuNen344 <s1100633@outlook.com>
2021-11-16 00:29:05 +0900

    bump kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0-RC2")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.0-RC2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.0")
         classpath("com.android.tools.build:gradle:7.0.3")
     }
 }
